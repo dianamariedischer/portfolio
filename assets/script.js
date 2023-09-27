03031e506236b7fb10289197156a7a48ff939088
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2023-09-27 02:56:45 -0400

    Added hamburger menu (still have to tweak), as well as external links and content blocks for work.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// typing function copied from w3
 var i = 0;
 var txt = "Hey! My name is Diana and I'm a web developer.";
 var speed = 50;
@@ -7,5 +8,15 @@ function typeWriter() {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  }
+}
+
+// hamburger menu from w3
+function myFunction() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
